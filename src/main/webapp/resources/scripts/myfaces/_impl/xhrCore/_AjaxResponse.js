77afb5462de278585677365c0c66b35e8da11c32
Werner Punz <werner.punz@gmail.com>
2011-10-13 11:56:19 +0200

    merge from svn head

--- src/main/webapp/resources/scripts/myfaces/_impl/xhrCore/_AjaxResponse.js
+++ src/main/webapp/resources/scripts/myfaces/_impl/xhrCore/_AjaxResponse.js
@@ -495,10 +495,6 @@ myfaces._impl.core._Runtime.singletonExtendClass("myfaces._impl.xhrCore._AjaxRes
                     newHead = doc.getElementsByTagName("head")[0];
                 }
 
-                //TODO we have to make a delta of styles we dont have anymore
-                //and remove those from the page
-                //the same goes for javascript
-
                 var oldTags = this._Dom.findByTagNames(document.getElementsByTagName("head")[0], {"link": true, "style":true});
                 this._Dom.runCss(newHead, true);
                 this._Dom.deleteItems(oldTags);
