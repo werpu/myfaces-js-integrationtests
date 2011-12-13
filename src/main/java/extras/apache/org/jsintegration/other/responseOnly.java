aafb4f19c47eaabe922344b648c5fa5d59869c6a
Werner Punz <werner.punz@gmail.com>
2011-12-13 10:24:24 +0100

    adding richfaces fileupload

---
+++ src/main/java/extras/apache/org/jsintegration/other/responseOnly.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package extras.apache.org.jsintegration.other;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+/**
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+@ManagedBean
+@RequestScoped
+public class ResponseOnly implements Serializable
+{
+    int cnt = 0;
+
+    public String doAction()
+    {
+        cnt++;
+        return null;
+    }
+
+    public String resetCnt()
+    {
+        cnt = 0;
+        return null;
+    }
+
+    public int getCnt()
+    {
+        return cnt;
+    }
+
+    public void setCnt(int cnt)
+    {
+        this.cnt = cnt;
+    }
+}
