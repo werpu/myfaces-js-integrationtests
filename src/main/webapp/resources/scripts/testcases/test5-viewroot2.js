f4f918cf2ad3d03a60c8e1e5f85e02dc9a92eb1e
Werner Punz <werner.punz@gmail.com>
2012-01-28 19:12:25 +0100

    fixing test5

--- src/main/webapp/resources/scripts/testcases/test5-viewroot2.js
+++ src/main/webapp/resources/scripts/testcases/test5-viewroot2.js
@@ -1,30 +1,33 @@
-var AjaxCase = myfaces._supportive.unittest.JSFAjaxTestCase;
+if (!AjaxCase) {
 
-var testGroup = new (_class("Test5ViewRoot",  myfaces._supportive.unittest.TestGroup,
-{
-    _description:"Chain Test",
-    constructor_: function() {
-        this._callSuper("constructor_");
-    },
-    tearDown: function() {
-        this._callSuper("tearDown");
-        this.autoForward("./test6-tablebasic.jsf");
-    }
-}))();
-testGroup.addCase(new AjaxCase({
-    description:"Chain test",
-    defer: 3000,
-    run: function() {
-        this.ajaxRequest("allKeyword", null, {
-            render:"@all", 
-            execute:"@all"
-        });
-    },
-    postcondition: function() {
-        this.assertTrue("resulting text found", $("body").html().indexOf("refresh successul2") != -1);
-    }
-}));
+    var AjaxCase = myfaces._supportive.unittest.JSFAjaxTestCase;
 
-setTimeout(function() {
-    testGroup.start();
-}, 1000);
+    var testGroup = new (_class("Test5ViewRoot", myfaces._supportive.unittest.TestGroup,
+            {
+                _description:"Chain Test",
+                constructor_:function () {
+                    this._callSuper("constructor_");
+                },
+                tearDown:function () {
+                    this._callSuper("tearDown");
+                    this.autoForward("./test6-tablebasic.jsf");
+                }
+            }))();
+    testGroup.addCase(new AjaxCase({
+        description:"Chain test",
+        defer:3000,
+        run:function () {
+            this.ajaxRequest("allKeyword", null, {
+                render:"@all",
+                execute:"@all"
+            });
+        },
+        postcondition:function () {
+            this.assertTrue("resulting text found", $("body").html().indexOf("refresh successul2") != -1);
+        }
+    }));
+
+    setTimeout(function () {
+        testGroup.start();
+    }, 1000);
+}
