705a906aa631d14b0665e92a9d77183d617896d5
Werner Punz <werner.punz@gmail.com>
2012-02-27 12:50:45 +0100

    initial commit windowid

--- src/main/webapp/resources/scripts/myfaces/_impl/_util/_DomExperimental.js
+++ src/main/webapp/resources/scripts/myfaces/_impl/_util/_DomExperimental.js
@@ -1,6 +1,6 @@
 if (_MF_SINGLTN) {
     _MF_SINGLTN(_PFX_UTIL + "_DomExperimental", myfaces._impl._util._Dom, /** @lends myfaces._impl._util._Dom.prototype */ {
-        constructor_: function() {
+        constructor_:function () {
             this._callSuper("constructor_");
             myfaces._impl._util._Dom = this;
         },
@@ -8,17 +8,70 @@ if (_MF_SINGLTN) {
         /**
          * fetches the window id for the current request
          * note, this is a preparation method for jsf 2.2
-         *
          */
-        getWindowId: function() {
-            var href = window.location.href;
-            var windowId = "windowId";
-            var regex = new RegExp("[\\?&]" + windowId + "=([^&#\\;]*)");
-            var results = regex.exec(href);
-            return (results != null) ? results[1] : null;
+        getWindowId:function (node) {
+            var fetchWindowIdFromForms = function (forms) {
+                var result_idx = {};
+                var result;
+                var foundCnt = 0;
+                for (var cnt = forms.length - 1; cnt >= 0; cnt--) {
+                    var currentForm = forms[cnt];
+                    var windowId = currentForm["javax.faces.WindowId"].value;
+                    if (windowId) {
+                        if (foundCnt > 0 && 'undefined' == typeof result_idx[windowId]) throw Error("Multiple different windowIds found in document");
+                        result = windowId;
+                        result_idx[windowId] = true;
+                        foundCnt++;
+                    }
+                }
+                return result;
+            }
+
+            var getChildForms = function (currentElement) {
+                var FORM = "form";
+                var targetArr = [];
+                if(!currentElement.tagName) return [];
+                else if (currentElement.tagName.toLowerCase() == FORM) {
+                    targetArr.push(currentElement);
+                    return targetArr;
+                }
+                //old recursive way, due to flakeyness of querySelectorAll
+                for (var cnt = currentElement.childNodes.length - 1; cnt >= 0; cnt--) {
+                    var currentChild = currentElement.childNodes[cnt];
+                    targetArr = targetArr.concat(getChildForms(currentChild, FORM));
+                }
+                return targetArr;
+            }
+
+            var findParentForms = function(element) {
+                while(element != null) {
+                    if(element.tagName.toLowerCase() == "form") return [element];
+                    element = element.parentNode;
+                }
+                return document.forms;
+            }
+
+            if (!node) {
+                var href = window.location.href;
+                var windowId = "windowId";
+                var regex = new RegExp("[\\?&]" + windowId + "=([^&#\\;]*)");
+                var results = regex.exec(href);
+                //initial trial over the url and a regexp
+                if (results != null) return results[1];
+                //second trial all forms
+                return fetchWindowIdFromForms(document.forms);
+            } else {
+                //check forms
+                var forms = getChildForms(node);
+                if(!forms || forms.length == 0) {
+                    //We walk up the parent until we hit a form or document.body
+                    forms = findParentForms(node);
+                }
+                return fetchWindowIdFromForms(forms);
+            }
         },
 
-        html5FormDetection: function(item) {
+        html5FormDetection:function (item) {
             var browser = this._RT.browser;
             //ie shortcut, not really needed but speeds things up
             if (browser.isIEMobile && browser.isIEMobile <= 8) {
@@ -28,17 +81,17 @@ if (_MF_SINGLTN) {
             return (elemForm) ? this.byId(elemForm) : null;
         },
 
-        isMultipartCandidate: function(executes) {
+        isMultipartCandidate:function (executes) {
             if (this._Lang.isString(executes)) {
                 executes = this._Lang.strToArray(executes, /\s+/);
             }
 
             for (var executable in executes) {
-                if(!executes.hasOwnProperty(executable)) continue;
+                if (!executes.hasOwnProperty(executable)) continue;
                 var element = this.byId(executes[executable]);
                 var inputs = this.findByTagName(element, "input", true);
                 for (var key in inputs) {
-                    if(!inputs.hasOwnProperty(key)) continue;
+                    if (!inputs.hasOwnProperty(key)) continue;
                     if (this.getAttribute(inputs[key], "type") == "file") return true;
                 }
             }
---
+++ src/main/webapp/resources/scripts/testcases/experimental/windowid.js
@@ -0,0 +1,109 @@
+var TestCase = myfaces._supportive.unittest.TestCase;
+var testGroup = new (_class("SeleniumGroup2", myfaces._supportive.unittest.TestGroup, {
+    _description:"Full Body Replacement",
+
+    constructor_:function () {
+        this._callSuper("constructor_");
+    },
+
+    setup:function () {
+        this._callSuper("setup");
+
+    },
+    postcondition:function () {
+        this._callSuper("postcondition");
+        return true;
+    },
+    tearDown:function () {
+        this._callSuper("tearDown");
+        //this.autoForward("./test3-viewbody1.jsf");
+    }
+
+}))();
+
+//testGroup.addStandardTestcase("Full Body Replacement normal", "form1", "body", function () {
+//    this.assertTrue("Body replacement and script eval performed", $("#scriptreceiver").html().indexOf("hello from embedded script") != -1);
+//});
+
+testGroup.addCase(new (_class("Case1", TestCase, {
+    _description:"Test for windowid",
+    _globalProcess:false,
+
+    constructor_:function () {
+        this._callSuper("constructor_");
+    },
+
+    run:function () {
+
+    },
+    postcondition:function () {
+        this._callSuper("postcondition");
+
+        try {
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("outerContainer"));
+            this.assertTrue("Result should be 10", parseInt(result) == 10);
+
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("centerForm"));
+            this.assertTrue("Result should be 10", parseInt(result) == 10);
+
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("centerForm2"));
+            this.assertTrue("Result should be 10", parseInt(result) == 10);
+
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("booga"));
+            this.assertTrue("Result should be 10", parseInt(result) == 10);
+
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("booga2"));
+            this.assertTrue("Result should be 10", parseInt(result) == 10);
+
+            return true;
+        } catch (e) {
+            this.fail("Error thrown");
+        }
+        return true;
+    },
+
+    tearDown:function () {
+        document.getElementById("booga2").value = document.getElementById("booga").value;
+        this._callSuper("tearDown");
+    }
+}))());
+
+testGroup.addCase(new (_class("Case2", TestCase, {
+    _description:"Test for double form error",
+    _globalProcess:false,
+
+    constructor_:function () {
+        this._callSuper("constructor_");
+    },
+    run:function () {
+    },
+    postcondition:function () {
+        this._callSuper("postcondition");
+
+        document.getElementById("booga2").value = "11";
+
+        try {
+            var dom = myfaces._impl._util._Dom;
+            var result = dom.getWindowId(document.getElementById("outerContainer"));
+            this.fail("window id should have thrown an error");
+            return false;
+        } catch (e) {
+            this.assertTrue("windowid has thrown an error legally", true);
+        }
+        return true;
+    },
+
+    tearDown:function () {
+        document.getElementById("booga2").value = document.getElementById("booga").value;
+        this._callSuper("tearDown");
+    }
+}))());
+
+setTimeout(function () {
+    testGroup.start();
+}, 100);
